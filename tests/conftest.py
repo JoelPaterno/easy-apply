4d3bb68e2b6660c74105b809bbdf998e78078518
joelpaterno <joelpaterno1@gmail.com>
2024-11-10 16:56:57 +1100

    tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,11 +5,6 @@ import pytest
 from easyapplyapp import create_app
 from easyapplyapp.db import db_session, init_db
 
-#load in slq to populate data base DELETE LATER
-with open(os.path.join(os.path.dirname(__file__), 'data.sql'), 'rb') as f:
-    _data_sql = f.read().decode('utf8')
-
-
 @pytest.fixture
 def app():
     db_fd, db_path = tempfile.mkstemp()
@@ -34,3 +29,19 @@ def client(app):
 @pytest.fixture
 def runner(app):
     return app.test_cli_runner()
+
+class AuthActions(object):
+    def __init__(self, client):
+        self._client = client
+    
+    def login(self, username='test', password='test'):
+        return self._client.post(
+            '/auth/login',
+            data={'username': username, 'password': password}
+        )
+    def logout(self):
+        return self._client.get('/auth/logout')
+
+@pytest.fixture
+def auth(client):
+    return AuthActions(client)
